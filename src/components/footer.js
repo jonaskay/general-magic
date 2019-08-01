f3369a1c2975cbdc7a2dc9f99960d5db2bde6b53
Tiina Romu <tiina.romu@wunderdog.fi>
2019-08-01 11:37:58 +0300

    Change styles in footer and change WD logo

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,34 +1,13 @@
 import React from "react"
-import { StaticQuery, graphql } from "gatsby"
-import Img from "gatsby-image"
 
 import footerStyles from "./footer.module.css"
 
 const Footer = () => (
-  <StaticQuery
-    query={graphql`
-      query {
-        footerImage: file(relativePath: { eq: "footer.jpg" }) {
-          childImageSharp {
-            fluid(maxWidth: 2560) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-      }
-    `}
-    render={data => (
-      <footer className={footerStyles.footer}>
-        <Img
-          fluid={data.footerImage.childImageSharp.fluid}
-          className={footerStyles.image}
-        />
-        <div className={footerStyles.copyright}>
-          © {new Date().getFullYear()}
-        </div>
-      </footer>
-    )}
-  />
+  <footer className={footerStyles.footer}>
+    <div className={footerStyles.copyright}>
+      © {new Date().getFullYear()}
+    </div>
+  </footer>
 )
 
 export default Footer
